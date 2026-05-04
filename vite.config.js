f2680a05f2d72f80d705d404c670705f24e2fabe
Chegemwa <victormwachege@gmail.com>
2026-05-04 15:20:07 +0300

    Add SEO meta tags and sitemap

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import sitemap from "vite-plugin-sitemap";
 
 export default defineConfig({
-  plugins: [
-    react(),
-  ],
-})
+  plugins: [react(), sitemap({ hostname: "https://vincera.co.ke" })],
+});
